0f6646bd53e805688d1be3cf97b2423239ef7bcf
yanni4night <670409732@qq.com>
2013-11-15 15:50:02 +0800

    support multiple cmds

--- Custominsert.py
+++ Custominsert.py
@@ -8,26 +8,46 @@ import sublime, sublime_plugin
 import re,datetime,os
 
 class CustominsertCommand(sublime_plugin.TextCommand):
+
+    def get_settings_param(self,chains=[],default=''):
+        '''get params in dict chains'''
+        obj=self.settings
+        for e in chains:
+            obj=obj.get(e)
+            if not obj:
+                return default
+        return obj
+
     def get_predefined_param(self,match):
         '''{%%}'''
         key=match.group(1)
         if key=='file_name':
             return os.path.basename(self.view.file_name() or '')
+        elif key=='file_path':
+            return self.view.file_name() or ''
         elif key=='datetime':
             t=datetime.datetime.today()  
-            return t.strftime(self.settings.get('__datetime_format__','%Y-%m-%d %H:%M:%S'))
+            return t.strftime(self.get_action_param('datetime_format','%Y-%m-%d %H:%M:%S'))
         return match.group(1)
+
     def get_defined_param(self,match):
         '''{{}} '''
         key=match.group(1)
-        return self.settings.get(key,"")
-    def run(self, edit):
+        return self.get_settings_param(['actions',self.action,'data',key],self.get_settings_param(['data',key]))
+
+    def get_action_param(self,key,default=''):
+        '''get params of actions'''
+        return self.get_settings_param(['actions',self.action,key],self.get_settings_param([key]))
+
+    def run(self, edit,action=''):
         v=self.view
         settings_name = 'Custominsert'
-        settings =sublime.load_settings(settings_name + '.sublime-settings')
+        settings =sublime.load_settings(settings_name + '.sublime-settings') or {}
         #save settings
         self.settings=settings
-        content=settings.get('__content__','__content__ NOT DEFINED')
+        #save action
+        self.action=action;
+        content=self.get_action_param('content')
         #replace {%%}
         p=re.compile('\{%\s*?([\w]+)\s*?%\}')
         content=re.sub(p,self.get_predefined_param,content)
@@ -35,7 +55,7 @@ class CustominsertCommand(sublime_plugin.TextCommand):
         p=re.compile('\{\{\s*?([\w]+)\s*?\}\}')
         content=re.sub(p,self.get_defined_param,content)
         #insert position
-        if 'start'==settings.get('__position__','start'):
+        if 'start'==self.get_action_param('position'):
             v.insert(edit, 0, content)
         else:
             v.insert(edit,v.sel()[0].begin(), content)
