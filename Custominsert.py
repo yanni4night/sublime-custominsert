dc38a7dd262223994b6fd90d45a4e20f61260b1e
yanni4night <yanni4night@gmail.com>
2013-12-30 12:11:52 +0800

    clean space

--- Custominsert.py
+++ Custominsert.py
@@ -20,86 +20,86 @@ def get_local_ip():
 
 class CustominsertCommand(sublime_plugin.TextCommand):
 
-    def get_settings_param(self,chains=[],default=''):
+    def get_settings_param(self,chains = [],default = ''):
         '''get params in dict chains'''
-        obj=self.settings
+        obj = self.settings
         for e in chains:
-            obj=obj.get(e)
+            obj = obj.get(e)
             if not obj:
                 return default
         return obj
 
     def get_predefined_param(self,match):
         '''{%%}'''
-        key=match.group(1)
-        if key=='filename':
+        key = match.group(1)
+        if key == 'filename':
             return os.path.basename(self.view.file_name() or '')
-        elif key=='filepath':
+        elif key == 'filepath':
             return self.view.file_name() or ''
-        elif key=='dirname':
+        elif key == 'dirname':
             return os.path.dirname(self.view.file_name() or '')
-        elif key=='platform':
+        elif key == 'platform':
             return sublime.platform()
-        elif key=='arch':
+        elif key == 'arch':
             return sublime.arch()
-        elif key=='encoding':
+        elif key == 'encoding':
             encoding = self.view.encoding()
             return encoding if 'Undefined' != encoding else self.settings.get('default_encoding')
-        elif key=='ip':
+        elif key == 'ip':
             return get_local_ip()
-            ipaddrlist=socket.gethostbyname_ex(socket.gethostname())
-            ips=''
+            ipaddrlist = socket.gethostbyname_ex(socket.gethostname())
+            ips = ''
             for e in ipaddrlist:
                 if isinstance(e,list):
                     for k in e:
-                        ips+=" "+k
+                        ips += " "+k
                 else:
-                    ips+=e
+                    ips += e
             return ips
-        elif key=='user':
-            user=os.getlogin() if 'windows' != sublime.platform() else ''
+        elif key == 'user':
+            user = os.getlogin() if 'windows' != sublime.platform() else ''
             if user:
                 return user
                 #windows?
-            user=os.popen('whoami').read()
-            p=re.compile('[\r\n]',re.M)
+            user = os.popen('whoami').read()
+            p = re.compile('[\r\n]',re.M)
             return re.sub(p,'',user)
-        elif key=='ext':
-            ext=os.path.splitext(self.view.file_name() or '')[1]
+        elif key == 'ext':
+            ext = os.path.splitext(self.view.file_name() or '')[1]
             #remove . at start position
-            p=re.compile('^\.')
+            p = re.compile('^\.')
             return re.sub(p,'',ext)
-        elif key=='datetime':
-            t=datetime.datetime.today()  
+        elif key == 'datetime':
+            t = datetime.datetime.today()  
             return t.strftime(self.get_action_param('datetime_format','%Y-%m-%d %H:%M:%S'))
         return match.group(1)
 
     def get_defined_param(self,match):
         '''{{}} '''
-        key=match.group(1)
+        key = match.group(1)
         return self.get_settings_param(['actions',self.action,'data',key],self.get_settings_param(['data',key]))
 
-    def get_action_param(self,key,default=''):
+    def get_action_param(self,key,default = ''):
         '''get params of actions'''
         return self.get_settings_param(['actions',self.action,key],self.get_settings_param([key]))
 
-    def run(self, edit,action=''):
-        v=self.view
+    def run(self, edit,action = ''):
+        v = self.view
         settings_name = 'Custominsert'
         settings =sublime.load_settings(settings_name + '.sublime-settings') or {}
         #save settings
-        self.settings=settings
+        self.settings = settings
         #save action
-        self.action=action;
-        content=self.get_action_param('content')
+        self.action = action;
+        content = self.get_action_param('content')
         #replace {%%}
-        p=re.compile('\{%\s*?([\w]+)\s*?%\}',re.LOCALE|re.MULTILINE)
-        content=re.sub(p,self.get_predefined_param,content)
+        p = re.compile('\{%\s*?([\w]+)\s*?%\}',re.LOCALE|re.MULTILINE)
+        content = re.sub(p,self.get_predefined_param,content)
         #replace {{}}
-        p=re.compile('\{\{\s*?([\w]+)\s*?\}\}',re.LOCALE|re.MULTILINE)
-        content=re.sub(p,self.get_defined_param,content)
+        p = re.compile('\{\{\s*?([\w]+)\s*?\}\}',re.LOCALE|re.MULTILINE)
+        content = re.sub(p,self.get_defined_param,content)
         #insert position
-        if 'start'==self.get_action_param('position'):
+        if 'start' == self.get_action_param('position'):
             v.insert(edit, 0, content)
         else:
             for sel in v.sel():
